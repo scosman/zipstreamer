ccac8b082ae133cfc1cddc834cc6b564e2b56ed0
scosman <scosman@users.noreply.github.com>
2023-01-26 10:51:29 -0500

    Return non zero exit code on unknown server errors

--- main.go
+++ main.go
@@ -28,9 +28,9 @@ func main() {
 		ReadTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Server starting on port %s", port)
 	shutdownChannel := make(chan os.Signal, 10)
 	go func() {
+		log.Printf("Server starting on port %s", port)
 		err := httpServer.ListenAndServe()
 		if err != nil {
 			log.Printf("Server Error: %s", err)
@@ -45,4 +45,9 @@ func main() {
 	shutdownSignal := <-shutdownChannel
 	log.Printf("Received signal (%s), shutting down...", shutdownSignal.String())
 	httpServer.Shutdown(context.Background())
+
+	// Exit was not expected, return non 0 exit code
+	if shutdownSignal == syscall.SIGUSR1 {
+		os.Exit(1)
+	}
 }
